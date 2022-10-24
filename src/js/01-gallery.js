fb094ef5b558d74ec78edad50f3d096bf5fabe25
Veronika Moskaliuk <veronika.moskaliuk.ua@gmail.com>
2022-10-24 21:13:41 +0200

    saved

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,35 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryContainer = document.querySelector(".gallery");
+const itemsMarkup = createGallaryItemMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function createGallaryItemMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image"
+        src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </div>
+      `;
+    })
+    .join("");
+}
+
+const gallery = SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
